ca98f9ed958399d516a1c17ee7ef8a9638ecdf45
Christian Dupuis <cd@atomist.com>
2016-12-02 12:37:16 +0100

    Don't write secrets to provenance info
    fixes #5

--- src/main/scala/com/atomist/project/ProvenanceInfoWriter.scala
+++ src/main/scala/com/atomist/project/ProvenanceInfoWriter.scala
@@ -4,23 +4,26 @@ import com.atomist.project.edit.ProjectEditor
 import com.atomist.project.generate.ProjectGenerator
 import com.atomist.source.{ArtifactSource, StringFileArtifact}
 import org.springframework.stereotype.Component
+import org.apache.commons.lang3.text.WordUtils
+import org.apache.commons.lang3.StringUtils
 
 @Component
 class ProvenanceInfoWriter {
 
-  val PROVENANCE_FILE: String = ".provenance.txt"
+  val ProvenanceFile: String = ".provenance.txt"
+  val SecretKeys: Seq[String] = Seq("password", "key", "secret", "token", "user")
 
   def write(projectSource: ArtifactSource, po: ProjectOperation, poa: ProjectOperationArguments): ArtifactSource = {
   
     val content = write(po, poa)
     
-    val provenanceFile = projectSource.findFile(PROVENANCE_FILE)
+    val provenanceFile = projectSource.findFile(ProvenanceFile)
     val updatedContent = provenanceFile match {
       case Some(existingContent) => existingContent.content + content
       case None => content
     }
 
-    projectSource.+(StringFileArtifact.apply(PROVENANCE_FILE, updatedContent))
+    projectSource.+(StringFileArtifact.apply(ProvenanceFile, updatedContent))
   }
   
   def write(po: ProjectOperation, poa: ProjectOperationArguments): String = {
@@ -48,9 +51,23 @@ class ProvenanceInfoWriter {
         content.append(s"sha: ${p.sha().getOrElse("n/a")}\n")
       case _ =>
     }
+    
+    def sanitizeValue(key: String, value: Any): Any = {
+      value match {
+        case s: String => 
+          if (SecretKeys.filter { sec => key.toLowerCase().contains(sec.toLowerCase()) }.isEmpty) {
+            s
+          }
+          else {
+            s.charAt(0) + ("*" * (s.length() - 2)) + s.last 
+          }
+        case _ => value
+      }
+      
+    }
 
     content.append("\n# parameter values\n")
-    poa.parameterValues.foreach(p => content.append(String.format("%s: %s\n", p.getName, p.getValue)))
+    poa.parameterValues.foreach(p => content.append(s"${p.getName}: ${sanitizeValue(p.getName, p.getValue)}\n"))
     content.append("\n")
     content.toString()
   }
