ed958670c34e4ec663553ea523c2c3eb0ac5cd5a
James Carnegie <james@atomist.com>
2017-04-12 21:10:07 +0100

    Use bouncy castle to verify gpg signatures

---
+++ src/test/scala/com/atomist/rug/resolver/GpgSignatureVerifierTest.scala
@@ -0,0 +1,36 @@
+package com.atomist.rug.resolver
+
+import java.io.File
+
+import org.scalatest.{DiagrammedAssertions, FunSpec, OneInstancePerTest}
+
+class GpgSignatureVerifierTest extends FunSpec with DiagrammedAssertions with OneInstancePerTest{
+
+  val verifier = new GpgSignatureVerifier(Thread.currentThread().getContextClassLoader.getResourceAsStream("atomist_pub.gpg"))
+
+  it("should verify files signed by atomist"){
+    assert(verifier.verify(
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar"),
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar.asc")
+    ))
+  }
+  it("should fail on files signed by atomist with corrupted signature"){
+    assert(!verifier.verify(
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar"),
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar.asc.corrupt")
+    ))
+  }
+  it("should fail if the jar as been modified") {
+    assert(!verifier.verify(
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT-corrupt.jar"),
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar.asc")
+    ))
+  }
+  it("should fail if the jar and signature is fine, but the cert is not the signing one") {
+    val verifier = new GpgSignatureVerifier(Thread.currentThread().getContextClassLoader.getResourceAsStream("other_pub.gpg"))
+    assert(!verifier.verify(
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar"),
+      new File("src/test/resources/rug-functions-github-0.14.2-SNAPSHOT.jar.asc")
+    ))
+  }
+}
