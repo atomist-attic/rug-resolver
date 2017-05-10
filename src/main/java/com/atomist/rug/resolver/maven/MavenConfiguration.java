f53a03b67a03b78bc575f8f6b71192918174aadc
Christian Dupuis <cd@atomist.com>
2017-05-10 23:32:15 +0200

    Make sure TakariUpdateCheckManager is service enabled

--- src/main/java/com/atomist/rug/resolver/maven/MavenConfiguration.java
+++ src/main/java/com/atomist/rug/resolver/maven/MavenConfiguration.java
@@ -7,9 +7,13 @@ import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.impl.SyncContextFactory;
+import org.eclipse.aether.impl.UpdateCheckManager;
+import org.eclipse.aether.impl.UpdatePolicyAnalyzer;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.spi.io.FileProcessor;
+import org.eclipse.aether.spi.locator.Service;
+import org.eclipse.aether.spi.locator.ServiceLocator;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,6 +23,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.atomist.rug.resolver.concurrent.MdcThreadPoolExecutor;
 
+import io.takari.aether.localrepo.TakariUpdateCheckManager;
 import io.takari.filemanager.FileManager;
 import io.takari.filemanager.internal.DefaultFileManager;
 
@@ -43,8 +48,16 @@ public class MavenConfiguration {
 
         locator.setService(SyncContextFactory.class, LockingSyncContextFactory.class);
         locator.setService(FileProcessor.class, LockingFileProcessor.class);
+        locator.setService(UpdateCheckManager.class, ServiceEnabledTakariUpdateCheckManager.class);
 
         return locator.getService(RepositorySystem.class);
     }
+    
+    public static class ServiceEnabledTakariUpdateCheckManager extends TakariUpdateCheckManager implements Service {
 
+        @Override
+        public void initService(ServiceLocator locator) {
+            setUpdatePolicyAnalyzer(locator.getService(UpdatePolicyAnalyzer.class));
+        }
+    }
 }
